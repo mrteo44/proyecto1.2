5b3fe1ea006f45db677c4b3adeb16dd5d2f099f4
mrteo44 <133880152+mrteo44@users.noreply.github.com>
2023-05-17 10:00:06 -0500

    Add files via upload

---
+++ tienda/vista/layouts/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=3.0">
+    <title >MVC Básico productos</title>
+    <link rel="stylesheet" href="vista/css/lol1.css" >
+</head>
+<body>
+   <div class="panel">
+       <h2 class="text-center" >🧺MICRO MERCADO TUTI TUTI🧺</h2>
+       <h1 class="text-center">🤙GESTIÓN DE PRODUCTOS🤙</h1>
+        <h1 class="text-center">🥝REALIZADO🥝</h1>
+        <h1 class="text-center"> POR </h1>
+        <h2 class="text-center" > "🤙MATEO IMBAQUIGO🤙"</h2>
+
+       <!-- contenido-->
